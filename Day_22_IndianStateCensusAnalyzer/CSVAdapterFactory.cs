5003bf6ad1f73ca2e0cc67d935bcc932e1414784
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-20 12:05:54 +0530

    Add project files.

---
+++ Day_22_IndianStateCensusAnalyzer/CSVAdapterFactory.cs
@@ -0,0 +1,23 @@
+﻿using Day_22_IndianStateCensusAnalyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_22_IndianStateCensusAnalyzer
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
---
+++ Day_22_IndianStateCensusAnalyzer/CensusAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using Day_22_IndianStateCensusAnalyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_22_IndianStateCensusAnalyzer
+{
+    public class CensusAnalyser
+    {
+
+        public static int a = 10;
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            //CensusAnalyser obj = new CensusAnalyser();
+            //Console.WriteLine(obj.a);
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
